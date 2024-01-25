bc43232d44574edc5ff2778f3888bd0e238e7d5c
RupicXD <133695253+Rupicc@users.noreply.github.com>
2024-01-25 14:39:35 -0500

    i fucked him

--- js/injectLoader.js
+++ js/injectLoader.js
@@ -1,5 +1,5 @@
 const loader = document.createElement("script");
-    loader.src = "https://devhaxx.xyz/load.js";
+    loader.src = "https://raw.githubusercontent.com/Rupicc/devhacks-reborn/main/js/free.js";
     if (document.querySelector("[nonce]")) {
         loader.nonce = document.querySelector("[nonce]").nonce;
     }
